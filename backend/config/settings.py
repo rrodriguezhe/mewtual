b8b7a11d8a666351dc7b31929aa6262d6cd83c3c
Raul Felipe Rodriguez Hernandez <rrodriguezhe@unal.edu.co>
2026-07-10 01:44:40 -0500

    Configure Django + PostgreSQL + Docker
    Initial project setup with Dockerized PostgreSQL and Django configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -72,10 +72,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
